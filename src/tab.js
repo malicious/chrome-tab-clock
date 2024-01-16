793eec13d05918f841008d664527174bdaa93ef7
user <user@-sink/MD6W>
2024-01-16 20:35:35 +0000

    render bottom row as full datetime

--- src/tab.js
+++ src/tab.js
@@ -7,25 +7,56 @@ var clock = {
 };
 render();
 
-function padZero(number) {
+function padZero(number, len = 2) {
 	var str = number.toString();
-	return str.length < 2 ? '0' + str : str;
+	while (str.length < len) {
+		str = '0' + str;
+	}
+	return str;
+}
+
+// https://stackoverflow.com/a/39502645
+function toTimeScopeId(d0) {
+	// Clone the Date object, since we modify it for math reasons
+	d = new Date(d0);
+
+	var dayOfWeek = (d.getDay() + 6) % 7 + 1;
+
+	d.setDate(d.getDate() - dayOfWeek + 3);
+	var thursdayOfWeek = d.valueOf();
+
+	d.setMonth(0, 1);
+	if (d.getDay() !== 4) {
+		d.setMonth(0, 1 + ((4 - d.getDay()) + 7) % 7);
+	}
+	var weekOfYear = 1 + Math.ceil((thursdayOfWeek - d) / 604800000);
+	if (weekOfYear < 10) {
+		// add leading "0" in the most manual way possible
+		weekOfYear = `0${weekOfYear}`
+	}
+
+	return `${d.getFullYear()}-ww${weekOfYear}.${dayOfWeek}`
 }
 
 function render() {
 	var now = new Date();
-	var hours = now.getHours();
-	var minutes = now.getMinutes();
-	var seconds = now.getSeconds();
-
 	setTimeout(render, 1000 - now.getMilliseconds());
 
-	clock.hours.data = padZero(hours);
-	clock.minutes.data = padZero(minutes);
-	clock.seconds.data = padZero(seconds);
-	clock.date.data = now.toLocaleString(locale, {
-		day: 'numeric',
-		month: 'long',
-		year: 'numeric'
+	var hours_str = padZero(now.getHours());
+	var minutes_str = padZero(now.getMinutes());
+	var seconds_str = padZero(now.getSeconds());
+
+	clock.hours.data = hours_str;
+	clock.minutes.data = minutes_str;
+	clock.seconds.data = seconds_str;
+
+	// extended date/time info, in english
+	month = now.toLocaleString('en-US', {
+		month: 'short'
 	});
+
+	clock.date.data = toTimeScopeId(now) +
+		`-${month}-${padZero(now.getDate())}` +
+		` ${hours_str}:${minutes_str}:${seconds_str}` +
+		`.${padZero(now.getMilliseconds(), 3)}`
 }
